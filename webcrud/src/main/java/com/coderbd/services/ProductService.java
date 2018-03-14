588ddac1e11d23a4ecb33f72059abaf1a4525540
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-03-15 02:45:36 +0600

    web crud

---
+++ webcrud/src/main/java/com/coderbd/services/ProductService.java
@@ -0,0 +1,14 @@
+package com.coderbd.services;
+
+
+import com.coderbd.domain.Product;
+
+public interface ProductService {
+    Iterable<Product> listAllProducts();
+
+    Product getProductById(Integer id);
+
+    Product saveProduct(Product product);
+
+    void deleteProduct(Integer id);
+}
